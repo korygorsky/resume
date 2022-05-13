32662f016f5a9a9ff8af49eb452cbd109ff8400c
Kory Gorsky <kory@gorsky.ca>
2022-05-13 15:08:09 -0400

    Fixing printing issue on vercel

--- pages/api/download.js
+++ pages/api/download.js
@@ -1,3 +1,4 @@
+import chromium from 'chrome-aws-lambda'
 import {
     initBrowser,
     generatePDF,
@@ -5,11 +6,17 @@ import {
 } from '@rossbob/html-to-pdf'
 
 export default async function handler(req, res) {
-    const browser = await initBrowser()
-
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+    })
     const url = 'http://localhost:3000' + (req.query.path ?? '/')
 
     const { pdf } = await generatePDF(browser, [{ url }])
+    await browser.close()
 
     res.setHeader('Content-Type', 'application/pdf')
     res.setHeader('Content-Disposition', 'attachment; filename=resume.pdf')
